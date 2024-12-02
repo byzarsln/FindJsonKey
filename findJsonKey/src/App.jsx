9c05abbee4bbe32f4b6cfc77425974e1f306dd96
Beyza Arslan <121162425+byzarsln@users.noreply.github.com>
2024-12-02 03:34:59 +0300

    +received JSON data with axios, useEffect +result value handled with split and reduce

--- findJsonKey/src/App.jsx
+++ findJsonKey/src/App.jsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
 
-  return (
+  const [inputValue, setInputValue] = useState("")
+  const [jsonValue, setJsonValue] = useState(null)
+  const [textAreaValue, setTextAreaValue] = useState("")
+
+  const findValue = async () => {
+    try {
+      const response = await axios.get("/pet3.json")
+      setJsonValue(response.data);
+
+    } catch (error) {
+      console.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    findValue()
+  }, [])
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    const result = inputValue.split(".").reduce((acc, key) => acc[key], jsonValue)
+    setTextAreaValue(JSON.stringify(result, null, 2))
+  }
+
+  return (  
     <div>
-      
     </div>
   )
 }
